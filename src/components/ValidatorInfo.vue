9fd15fb3e59da8e3ef5dea759ea7bf4703681c26
Nikita <nikita1tup@gmail.com>
2022-06-02 14:43:21 +0300

    added new style in validator item, added new endpoint in oracle reports

---
+++ src/components/ValidatorInfo.vue
@@ -0,0 +1,254 @@
+<template>
+  <div class="validator-info">
+    <div class="validator-info__top-line">
+      <div class="validator-info__top-line-item card-frame">
+        <span class="validator-info__top-line-item-title">Moniker</span>
+        <div class="validator-info__card-balance-row-value-wrapper">
+          <span class="validator-info__top-line-item-value">
+            {{ validator.description.moniker }}
+          </span>
+        </div>
+      </div>
+      <div class="validator-info__top-line-item card-frame">
+        <span class="validator-info__top-line-item-title">Status</span>
+        <div class="validator-info__card-balance-row-value-wrapper">
+          <span class="validator-info__top-line-item-value">
+            {{ $tBondStatus(validator.status) }}
+          </span>
+        </div>
+      </div>
+      <div class="validator-info__top-line-item card-frame">
+        <span class="validator-info__top-line-item-title">Jailed?</span>
+        <div class="validator-info__card-balance-row-value-wrapper">
+          <span class="validator-info__top-line-item-value">
+            {{ validator.jailed ? 'Yes' : 'No' }}
+          </span>
+        </div>
+      </div>
+      <div class="validator-info__top-line-item card-frame">
+        <span class="validator-info__top-line-item-title">Stake</span>
+        <div class="validator-info__card-balance-row-value-wrapper">
+          <span class="validator-info__top-line-item-value">
+            {{ $convertLokiToOdin(validator.tokens, { withDenom: true }) }}
+          </span>
+        </div>
+      </div>
+      <div class="validator-info__top-line-item card-frame">
+        <span class="validator-info__top-line-item-title">Rate</span>
+        <div class="validator-info__card-balance-row-value-wrapper">
+          <span class="validator-info__top-line-item-value">
+            {{ $getPrecisePercents(validator.commission.commissionRates.rate) }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="validator-info__table">
+      <div class="validator-info__description card-frame">
+        <div class="validator-info__description-title">Information</div>
+        <div class="validator-info__description-item">
+          <span class="validator-info__description-item-title"
+            >Delegator shares</span
+          >
+          <span class="validator-info__description-item-value">
+            {{
+              $getPercentOutOfNumber(
+                validator.delegatorShares,
+                validator.tokens
+              )
+            }}
+          </span>
+        </div>
+        <div class="validator-info__description-item">
+          <span class="validator-info__description-item-title"
+            >Amount of create blocks</span
+          >
+          <span class="validator-info__description-item-value" :title="0">
+            {{ 0 }}
+          </span>
+        </div>
+        <div class="validator-info__description-item">
+          <span class="validator-info__description-item-title">Max rate</span>
+          <span class="validator-info__description-item-value">
+            {{
+              $getPrecisePercents(validator.commission.commissionRates.maxRate)
+            }}
+          </span>
+        </div>
+        <div class="validator-info__description-item">
+          <span class="validator-info__description-item-title"
+            >Max change rate</span
+          >
+          <span class="validator-info__description-item-value">
+            {{
+              $getPrecisePercents(
+                validator.commission.commissionRates.maxChangeRate
+              )
+            }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ValidatorDecoded } from '@/helpers/validatorDecoders'
+
+export default defineComponent({
+  components: {},
+  props: {
+    validator: { type: Object as PropType<ValidatorDecoded>, required: true },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.validator-info {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 4rem;
+  gap: 4rem;
+}
+.validator-info__table {
+  display: flex;
+  gap: 2rem;
+}
+.validator-info__delegetion,
+.validator-info__description {
+  width: 100%;
+}
+.validator-info__top-line {
+  display: grid;
+  gap: 2.4rem;
+  grid:
+    auto /
+    minmax(4rem, 1fr)
+    minmax(4rem, 1fr)
+    minmax(4rem, 1fr)
+    minmax(8rem, 1fr)
+    minmax(8rem, 1fr);
+}
+.validator-info__top-line-item {
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  max-width: 100%;
+}
+
+.validator-info__row,
+.validator-info__delegetion-btn-wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+}
+
+.validator-info__top-line-item-title {
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+  margin-bottom: 0.4rem;
+}
+.validator-info__top-line-item-value {
+  font-weight: 600;
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.validator-info__delegetion-title,
+.validator-info__description-title {
+  font-weight: 400;
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  margin-bottom: 3.2rem;
+}
+.validator-info__description-item {
+  display: flex;
+  &:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+}
+.validator-info__description-item-title {
+  font-size: 1.6rem;
+  line-height: 2.4rem;
+  min-width: 17rem;
+  max-width: 20rem;
+  width: 100%;
+}
+.validator-info__description-item-value {
+  font-size: 1.4rem;
+  line-height: 2rem;
+}
+
+.validator-info__delegetion-balance {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 3.2rem;
+}
+.validator-info__delegetion-balance-row {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1.6rem 0;
+  min-width: 10rem;
+  &:not(:last-child) {
+    border-right: 0.1rem solid var(--clr__input-border);
+    margin-right: 2rem;
+    padding-right: 1.6rem;
+  }
+}
+.validator-info__card-balance-row-value-wrapper {
+  display: flex;
+  flex-direction: column;
+  max-width: 20rem;
+  position: relative;
+}
+.validator-info__card-balance-row-value {
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  width: 100%;
+  font-weight: 600;
+  color: var(--clr__text);
+}
+.validator-info__delegetion-btn {
+  min-width: 9.9rem;
+  max-width: 10.8rem;
+  width: 100%;
+}
+@media screen and (max-width: 768px) {
+  .validator-info {
+    gap: 2.4rem;
+  }
+
+  .validator-info__top-line {
+    grid-template-columns: 1fr 1fr;
+    gap: 1.6rem;
+  }
+  .validator-info__table {
+    flex-direction: column;
+    gap: 2rem;
+  }
+  .validator-info__delegetion-btn-wrapper {
+    gap: 0.8rem;
+  }
+  .validator-info__top-line-item-value {
+    font-size: 2rem;
+    line-height: 2.4rem;
+  }
+}
+@media screen and (max-width: 393px) {
+  .validator-info__delegetion-btn-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.8rem;
+  }
+  .validator-info__delegetion-btn {
+    max-width: none;
+  }
+}
+</style>
--- src/views/ValidatorItem.vue
+++ src/views/ValidatorItem.vue
@@ -1,37 +1,48 @@
 <template>
   <div class="app__main-view validator-item">
-    <div class="app__main-view-title-wrapper">
-      <BackButton :text="'Validators'" />
-      <h2 class="app__main-view-title validator-item__title">Validator</h2>
-      <div
-        class="app__main-view-subtitle-wrapper validator-item__subtitle-wrapper"
-        v-if="validator"
-      >
-        <p class="app__main-view-subtitle fs-cut">
-          {{ validator.operatorAddress }}
+    <div class="app__main-view-title-wrapper validators-item__title-wrapper">
+      <BackButton class="validators-item__back-btn" text="Oracle validators" />
+      <h2 class="app__main-view-title validators-item__title">Validator</h2>
+      <div class="validators-item__validator-address">
+        <p
+          :title="validator?.operatorAddress"
+          class="view-main__subtitle validators-item__subtitle"
+        >
+          {{ validator?.operatorAddress }}
         </p>
-        <CopyButton class="mg-l8" :text="String(validator.operatorAddress)" />
+        <CopyButton class="mg-l8" :text="String(validator?.operatorAddress)" />
+      </div>
+      <div class="validators-item__oracle-status">
+        <StatusIcon
+          :width="14"
+          :height="14"
+          class="validators-item__oracle-status-icon"
+          :status="validator?.isActive ? 'success' : 'error'"
+        />
+
+        <span
+          class="validators-item__oracle-status-text"
+          :class="
+            validator?.isActive
+              ? 'validators-item__oracle-status-text--success'
+              : 'validators-item__oracle-status-text--error'
+          "
+          >Oracle</span
+        >
       </div>
     </div>
 
     <template v-if="validator">
-      <ValidatorInfoCard :validator="validator" />
-
+      <ValidatorInfo :validator="validator" />
       <AppTabs>
         <AppTab title="Oracle Reports">
-          <template v-if="reports">
-            <OracleReportsTable :reports="reports" />
-          </template>
+          <OracleReportsTable :proposerAddress="validator.operatorAddress" />
         </AppTab>
-        <AppTab title="Delegators">
-          <template v-if="delegators">
-            <DelegatorsTable :delegators="delegators" />
-          </template>
+        <AppTab :title="delegatorsTitle">
+          <DelegatorsTable :delegators="delegators" />
         </AppTab>
         <AppTab title="Proposed Blocks">
-          <template v-if="blocks">
-            <ProposedBlocksTable :proposerAddress="validator.operatorAddress" />
-          </template>
+          <ProposedBlocksTable :proposerAddress="validator.operatorAddress" />
         </AppTab>
       </AppTabs>
     </template>
@@ -42,21 +53,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref, computed } from 'vue'
 import { RouteLocationNormalizedLoaded, useRoute } from 'vue-router'
 import { callers } from '@/api/callers'
-import { Bech32 } from '@cosmjs/encoding'
 import BackButton from '@/components/BackButton.vue'
 import CopyButton from '@/components/CopyButton.vue'
 import AppTabs from '@/components/tabs/AppTabs.vue'
 import AppTab from '@/components/tabs/AppTab.vue'
-import ValidatorInfoCard from '@/components/ValidatorInfoCard.vue'
+import ValidatorInfo from '@/components/ValidatorInfo.vue'
 import OracleReportsTable from '@/components/tables/OracleReportsTable.vue'
 import DelegatorsTable from '@/components/tables/DelegatorsTable.vue'
 import ProposedBlocksTable from '@/components/tables/ProposedBlocksTable.vue'
-import { BlockResponse } from '@cosmjs/tendermint-rpc'
-import { blocksWithTotalTxInterface } from '@/helpers/Types'
 import { handleError } from '@/helpers/errors'
+import { DelegationResponse } from 'cosmjs-types/cosmos/staking/v1beta1/staking'
+import StatusIcon from '@/components/StatusIcon.vue'
+import { isActiveValidator } from '@/helpers/validatorDecoders'
 
 export default defineComponent({
   components: {
@@ -64,69 +75,43 @@ export default defineComponent({
     CopyButton,
     AppTabs,
     AppTab,
-    ValidatorInfoCard,
+    ValidatorInfo,
     OracleReportsTable,
     DelegatorsTable,
     ProposedBlocksTable,
+    StatusIcon,
   },
   setup: function () {
     const route: RouteLocationNormalizedLoaded = useRoute()
     const validator = ref()
-    const delegators = ref()
-    const blocks = ref()
-    const reports = ref()
+    const delegators = ref<DelegationResponse[]>([])
+    const delegatorsTitle = computed(() =>
+      delegators.value?.length
+        ? `Delegators (${delegators.value?.length})`
+        : 'Delegators'
+    )
 
     const getValidator = async () => {
       const response = await callers.getValidator(String(route.params.address))
-      validator.value = { ...response.validator }
+      validator.value = {
+        ...response.validator,
+        isActive: await isActiveValidator(String(route.params.address)),
+      }
     }
 
     const getDelegators = async () => {
       const response = await callers.getValidatorDelegations(
         String(route.params.address)
       )
-      delegators.value = [...response.delegationResponses]
-    }
-
-    const getBlocks = async () => {
-      const response = await callers.getBlockchain()
-      blocks.value = response.blockMetas.filter((item) => {
-        const encodedAddress = Bech32.encode(
-          'odinvaloper',
-          item.header.proposerAddress
-        )
-
-        if (encodedAddress === String(route.params.address)) return true
-        return false
-      })
-
-      let tempA: Array<blocksWithTotalTxInterface> = []
-      for (let b of blocks.value) {
-        tempA = [
-          ...tempA,
-          {
-            ...b,
-            total_tx: await callers
-              .getBlock(b?.header?.height as number)
-              .then((res: BlockResponse): number => res?.block?.txs?.length),
-          },
-        ]
+      if (response.delegationResponses) {
+        delegators.value = response.delegationResponses
       }
-
-      blocks.value = tempA
-    }
-
-    const getReports = async () => {
-      // TODO get the necessary reports from transactions
-      reports.value = []
     }
 
     onMounted(async () => {
       try {
         await getValidator()
         await getDelegators()
-        await getBlocks()
-        await getReports()
       } catch (error) {
         handleError(error as Error)
       }
@@ -135,29 +120,109 @@ export default defineComponent({
     return {
       validator,
       delegators,
-      blocks,
-      reports,
+      delegatorsTitle,
     }
   },
 })
 </script>
 
 <style lang="scss" scoped>
-.validator-item__title {
+.validators-item__title-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  margin-right: 2rem;
+  font-size: 2.4rem;
+}
+.validators-item__title {
   margin: 0 1.6rem 0 2rem;
 }
-
-.validator-item__subtitle-wrapper {
+.validators-item__validator-address {
   display: flex;
+  min-width: 10%;
+  margin-right: 1rem;
 }
-
+.validators-item__back-btn {
+  height: 4.6rem;
+  font-size: 16px;
+  line-height: 24px;
+}
+.validators-item__oracle-status {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: var(--clr__modal-field-bg);
+  border-radius: 3.2rem;
+  width: 7.8rem;
+  height: 2.8rem;
+  padding: 0.4rem 1.2rem 0.4rem 0.8rem;
+  gap: 0.4rem;
+  margin-right: 2rem;
+}
+.validators-item__oracle-status-text {
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 2rem;
+  &--success {
+    color: var(--clr__oracle-status-success);
+  }
+  &--error {
+    color: var(--clr__oracle-status-error);
+  }
+}
+.validators-item__subtitle {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  line-height: 4.6rem;
+}
 @include respond-to(tablet) {
-  .validator-item__title {
+  .validators-item__title-wrapper {
+    align-items: flex-start;
+  }
+  .validators-item {
+    padding-bottom: 10rem;
+  }
+  .validators-item__title {
     margin: 0.8rem 0 0.4rem 0;
   }
 
-  .validator-item__subtitle-wrapper {
+  .validators-item__subtitle {
+    line-height: 3rem;
+  }
+
+  .validators-item__back-btn {
+    height: 2.4rem;
+  }
+
+  .validators-item__validator-address {
     width: 100%;
+    margin: 0;
+  }
+  .validators-item__activities--top {
+    display: none;
+  }
+
+  .validators-item--large-padding {
+    padding-bottom: 17rem;
+  }
+  .validators-item__title-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-right: 0;
+    margin-bottom: 3.2rem;
+  }
+  .validators-item__oracle-status {
+    margin-top: 1.2rem;
+  }
+}
+
+@include respond-to(small) {
+  .validators-item__activities-btn {
+    font-size: 1.6rem;
   }
 }
 </style>
